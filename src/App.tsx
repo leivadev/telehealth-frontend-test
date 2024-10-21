9d53c66ca511fe5812e6885a66f9509a4cd65c03
Luislev <luis.leiva@utp.ac.pa>
2024-10-21 17:26:12 -0500

    refactor: change page components naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import SideNavbar from './components/SideNavbar';
-import ClinicsHome from './pages/ClinicsHome';
-import AddClinic from './pages/AddClinic';
+import ClinicsHomePage from './pages/ClinicsHomePage';
+import AddClinicPage from './pages/AddClinicPage';
 
 function App() {
   return (
     <div className="app-container grid grid-cols-[1fr_5fr] h-screen">
       <SideNavbar />
-      <div className="main-content p-4">
-        <Routes>
-          <Route path="/" element={<ClinicsHome />} />
-          <Route path="add-clinic" element={<AddClinic />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<ClinicsHomePage />} />
+        <Route path="add-clinic" element={<AddClinicPage />} />
+      </Routes>
     </div>
   );
 }
